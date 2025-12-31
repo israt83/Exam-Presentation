e106a9a9c5a98bb3063d14d1ae98654fdd7cf350
Israt Jahan Jisha <isratjahanfci@gmail.com>
2025-12-31 22:05:09 +0600

    update server.ts file

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -8,7 +8,7 @@ import cors from 'cors';
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "https://registration-from-pink.vercel.app" }));
 
 connectDB();
 
